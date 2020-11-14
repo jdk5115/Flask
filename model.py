f97bc7e902765d941a6c98be5c624371810c6e0f
Jonathan Kelly <jdk5115@gmail.com>
2020-11-14 17:58:24 -0500

    total mess

--- model.py
+++ model.py
@@ -12,6 +12,22 @@ def my_name(username):
     message = '{username}\'s password is {password}.'.format(username=username, password=password)
     return password, message
 
+def check_users():
+    connection = sqlite3.connect('todo.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT username FROM users ORDER BY pk DESC;""")
+    db_users = cursor.fetchall()
+    users = []
+
+    for i in range(len(db_users)):
+        person = db_users[i][0]
+        users.append(person)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return users
+
 def check_pw(username):
     connection = sqlite3.connect('todo.db', check_same_thread=False)
     cursor = connection.cursor()
@@ -31,7 +47,7 @@ def signup(username, password, favorite_color):
     exist = cursor.fetchone()
 
     if exist is None:
-        cursor.execute(""" INSERT INTO users(username, password, favorite_color) VALUES('{username}','{password}','{favorite_color}';""".format(username=username, password=password, favorite_color=favorite_color))
+        cursor.execute(""" INSERT INTO users(username, password, favorite_color) VALUES('{username}','{password}','{favorite_color}');""".format(username=username, password=password, favorite_color=favorite_color))
     else:
         return ('User already exists.')  
     
